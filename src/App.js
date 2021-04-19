d1de3c21e00089a1ae12d161745f8354840d6137
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2021-04-19 16:28:54 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Game from './components/Game.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Tic Tac Toe
       </header>
+      <div className="App-content">
+        <Game />
+      </div>
+      <footer className="App-footer">
+        Copyright 2021.
+      </footer>
     </div>
   );
 }
